e39a8754cea575de2566a536c0834708383eb2d9
Steven Schoen <digisteve5@gmail.com>
2026-01-30 19:55:13 -0800

    Add params

--- player.gd
+++ player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@export var move_speed := 600
+@export var jump_force := 1000
+
 func _process(delta: float) -> void:
 	pass
 
@@ -7,13 +10,13 @@ func _physics_process(delta: float) -> void:
 	var gravity := 50
 	var move_input := Input.get_vector(\
 		"Move left", "Move right", "Nothing", "Nothing")
-	velocity.x = move_input.x * 600.0
+	velocity.x = move_input.x * move_speed
 	
 	if !is_on_floor():
 		velocity.y += gravity
 	
 	if Input.is_action_just_pressed("Jump") && is_on_floor():
 		print(is_on_floor())
-		velocity.y = -1000
+		velocity.y = -jump_force
 	
 	move_and_slide()
