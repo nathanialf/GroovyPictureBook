52c052e15964dc6999aa54d128531931415c0579
Johnathon Van Atta <jvanatt3@binghamton.edu>
2026-02-01 13:58:23 -0800

    Initial end code
    Initial end  code

--- models/pushpin_managers.gd
+++ models/pushpin_managers.gd
@@ -3,7 +3,19 @@ extends Node3D
 @export var pins: Array[Node3D]
 @export var pickup_dist = 1.0
 @export var ui_pins: Array[TextureRect]
+@export var player: Node3D
+@export var edge_obj: Node3D
+@export var edge_obj_dist := 1.0
 
+var finished = false
+var remove_time = 0.0
+var inital_pos = Vector3()
+var inital_rot = Vector3()
+
+func _ready() -> void:
+	inital_pos = edge_obj.position
+	inital_rot = edge_obj.rotation
+	
 func _process(delta: float) -> void:
 	var pin_num = 0
 	
@@ -20,3 +32,32 @@ func _process(delta: float) -> void:
 				ui_pins[pin_index].position.y = 29.0
 			else: 
 				ui_pins[pin_index].position.y = -129.0
+	
+	if pin_num==0:
+		var diff_vec = Vector2(player.position.x, player.position.y) - Vector2(edge_obj.position.x, edge_obj.position.y)
+		var diff = diff_vec.length()
+		
+		if diff < edge_obj_dist:
+			finished = true
+		
+	if finished:
+		remove_time += delta + clamp((remove_time/1000.0), 0.0, delta*2)
+		
+		var shake_amt = (sin(remove_time*25)+(sin(remove_time*55.3253)/2.0)) * \
+										 (sqrt(remove_time/2.0)/10.0)
+		
+		var pop_amt = 0
+		
+		if remove_time>2.0:
+			var offset_time = remove_time-2.0
+			
+			pop_amt = -offset_time*15.0
+			
+			var pop_y_i = ((2*offset_time) - 0.707)
+			var pop_y = -(pop_y_i*pop_y_i)+0.5
+			
+			edge_obj.position = inital_pos + Vector3(offset_time,pop_y,0)
+			
+		
+		edge_obj.rotation = Vector3(0,0,shake_amt+pop_amt)
+		
