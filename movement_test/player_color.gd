9522afeaf7654305c6430e87020a33799cacd1a6
Johnathon Van Atta <jvanatt3@binghamton.edu>
2026-02-01 17:41:19 -0800

    player_color_fix
    player_color_fix

--- movement_test/player_color.gd
+++ movement_test/player_color.gd
@@ -1,10 +1,10 @@
 extends MeshInstance3D
 
-@export var colors: Array[Vector3]
+@export var materials: Array[Mater]
 
 var time = 0.0
 
 func _process(delta: float) -> void:
 	time+=delta
 	
-	
+	set_surface_override_material()
